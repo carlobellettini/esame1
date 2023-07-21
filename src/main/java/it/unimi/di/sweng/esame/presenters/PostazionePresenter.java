15bc2270da1906de4be346ca85528835a67eab3b
Carlo Bellettini <carlo.bellettini@unimi.com>
2023-07-21 10:44:56 +0200

    VERDE: check nome vuoto dal presenter

--- src/main/java/it/unimi/di/sweng/esame/presenters/PostazionePresenter.java
+++ src/main/java/it/unimi/di/sweng/esame/presenters/PostazionePresenter.java
@@ -3,11 +3,14 @@ package it.unimi.di.sweng.esame.presenters;
 import it.unimi.di.sweng.esame.views.PostazioneView;
 
 public class PostazionePresenter implements Presenter{
+  private final PostazioneView view;
+
   public PostazionePresenter(PostazioneView view) {
+    this.view = view;
   }
 
   @Override
   public void action(String text1, String text2) {
-
+    view.showError("nome vuoto");
   }
 }
