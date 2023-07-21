d2e98687e22c024b736ba00eaaa406eb0f67d308
Carlo Bellettini <carlo.bellettini@unimi.com>
2023-07-21 12:29:40 +0200

    VERDE: check bagnino arriva in postazione occupata

--- src/main/java/it/unimi/di/sweng/esame/model/Modello.java
+++ src/main/java/it/unimi/di/sweng/esame/model/Modello.java
@@ -2,16 +2,16 @@ package it.unimi.di.sweng.esame.model;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Modello {
   private final @NotNull Map<Bagnino, Postazione> postazioni = new HashMap<>();
+  private final @NotNull Set<Area> areeOccupate = new HashSet<>();
 
   public void arriva(@NotNull Bagnino bagnino, @NotNull Area area) {
+    if (areeOccupate.contains(area)) throw new IllegalArgumentException("postazione già occupata");
     postazioni.put(bagnino, new Postazione(bagnino, area, Bandiera.NONE));
+    areeOccupate.add(area);
   }
 
   public @NotNull List<Postazione> getListaPostazioni() {
