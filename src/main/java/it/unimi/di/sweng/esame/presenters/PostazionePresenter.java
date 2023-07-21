f10a8670c86c4a10095def39e4faad5802a6f680
Carlo Bellettini <carlo.bellettini@unimi.com>
2023-07-21 10:43:38 +0200

    ROSSO: check nome vuoto dal presenter

---
+++ src/main/java/it/unimi/di/sweng/esame/presenters/PostazionePresenter.java
@@ -0,0 +1,13 @@
+package it.unimi.di.sweng.esame.presenters;
+
+import it.unimi.di.sweng.esame.views.PostazioneView;
+
+public class PostazionePresenter implements Presenter{
+  public PostazionePresenter(PostazioneView view) {
+  }
+
+  @Override
+  public void action(String text1, String text2) {
+
+  }
+}
--- src/test/java/it/unimi/di/sweng/esame/VariTest.java
+++ src/test/java/it/unimi/di/sweng/esame/VariTest.java
@@ -2,9 +2,26 @@ package it.unimi.di.sweng.esame;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import it.unimi.di.sweng.esame.presenters.PostazionePresenter;
+import it.unimi.di.sweng.esame.views.PostazioneView;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 
 class VariTest {
 
+  @Test
+  void presenterCheckNomeVuoto() {
+    PostazioneView view = mock(PostazioneView.class);
+    PostazionePresenter SUT = new PostazionePresenter(view);
+    SUT.action("Arrriva", "");
+
+    verify(view).showError("nome vuoto");
+
+
+  }
 }
